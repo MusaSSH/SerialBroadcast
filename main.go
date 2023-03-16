1c6f94aae28868b46273c3c57d849363c2119877
Musa <97892949+MusaSSH@users.noreply.github.com>
2023-03-16 23:27:49 +0300

    init

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.bug.st/serial"
+	"go.bug.st/serial/enumerator"
+)
+
+var selectedPort *enumerator.PortDetails
+var buff bytes.Buffer
+
+func main() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+
+	fmt.Println("Please indicate the number or name of the serial port you want to connect:")
+	for i, p := range ports {
+		if p.IsUSB {
+			fmt.Printf("%d: %s\n", i, p.Name)
+		}
+	}
+
+	fmt.Print("Input: ")
+	var selection string
+	_, err = fmt.Scanln(&selection)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, p := range ports {
+		if p.IsUSB && (selection == p.Name || selection == fmt.Sprintf("%d", i)) {
+			selectedPort = p
+			fmt.Println("Selected port:", p.Name)
+			break
+		}
+	}
+
+	port, err := serial.Open(selectedPort.Name, &serial.Mode{
+		BaudRate: 9600,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go serialRead(port)
+
+	s := <-sig
+	port.Close()
+	fmt.Println("Signal:", s)
+}
+
+func serialRead(port serial.Port) {
+	for {
+		read := make([]byte, 128)
+		_, err := port.Read(read)
+		if err != nil {
+			log.Println(err)
+		}
+		read = bytes.Trim(read, "\x00")
+		buff.Write(read)
+
+		var messages []string
+		for {
+			s, err := buff.ReadString('\n')
+			if err == io.EOF {
+				buff.Reset()
+				buff.Write([]byte(s))
+				break
+			}
+			messages = append(messages, s)
+		}
+
+		if len(messages) > 0 {
+			for _, m := range messages {
+				fmt.Println("Message:", m)
+			}
+		}
+	}
+}
