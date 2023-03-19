c78c91d23d0bcd12ab932db5b443ac26951b104d
Musa <97892949+MusaSSH@users.noreply.github.com>
2023-03-19 21:11:21 +0300

    Remove amqp configs & add websocket

--- config/config.go
+++ config/config.go
@@ -8,10 +8,7 @@ import (
 type Config struct {
 	SerialPort string `envconfig:"SERIAL_PORT" required:"true"`
 	BaudRate   int    `envconfig:"BAUD_RATE" default:"9600"`
-
-	AmqpAddr      string `envconfig:"AMQP_ADDR" required:"true"`
-	AmqpTtl       int    `envconfig:"AMQP_TTL" default:"1000"`
-	AmqpQueueName string `envconfig:"AMQP_QUEUE_NAME" default:"serialPort"`
+	WSPort     string `envconfig:"WS_PORT" default:":1234"`
 }
 
 func Build() fx.Option {
